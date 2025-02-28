4200cfb29125e925b1b6f56392f5e7727588a5ff
quentin.mazou <quentin.mazou@git.forge.epita.fr>
2025-02-28 11:41:26 +0000

    second commit

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.helloworld;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +21,11 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Toast toast = Toast.makeText(getApplicationContext(),
+                "onCreate Called", Toast.LENGTH_LONG);
+        toast.show();
     }
+
+
 }
